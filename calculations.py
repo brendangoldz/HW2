9fd01a7cc8cb4aa6462be9561f1b7d519d439750
Brendan Goldsmith <brendangoldz@gmail.com>
2022-04-18 22:15:47 -0400

    Weight, Bias, Prob Calcing
    Cost not producing proper shape

--- calculations.py
+++ calculations.py
@@ -32,6 +32,8 @@ class Calculations():
     # TP = ((predictions == 1) & (true_values == 1)).sum()
     # FP = ((predictions == 1) & (true_values == 0)).sum()
     # precision = TP / (TP+FP)
+    def true_negatives(self, Y, Y_):
+        return ((Y_ == 0) & (Y == 1)).sum()
     
     def true_positives(self, Y, Y_):
         return ((Y_ == 1) & (Y == 1)).sum()
@@ -39,12 +41,21 @@ class Calculations():
     def false_positives(self, Y, Y_):
         return ((Y_ == 1) & (Y == 0)).sum()
     
+    def false_negatives(self, Y, Y_):
+        return ((Y_ == 0) & (Y == 1)).sum()
+    
+    def recall(self, Y, Y_):
+        TP = true_positives(Y, Y_)
+        FN = false_negatives(Y, Y_)
+        return (TP)/(TP+FN)
+    
     def precision(self, Y, Y_):
         TP = true_positives(Y, Y_)
-        return (TP)/(TP+false_positives(Y, Y_))
+        FP = false_positives(Y, Y_)
+        return (TP)/(TP+FP)
 
     def fmeasure(self):
-        return 0
+        return (2*precision(Y,Y_)*recall(Y,Y_))/(precision(Y,Y_)+recall(Y,Y_))
 
     def accuracy(self, Y, Y_h):
         return 100-((Y == Y_h).sum()/(Y.shape[0]))
--- log_regression.py
+++ log_regression.py
@@ -11,7 +11,9 @@ class LogisticalRegression():
     LEARNING_RATE = 0.005
 
     def linear_mod(self, w, X, b):
-        return np.dot(X, w) + b
+        # print(w.shape)
+        # print(X.T.shape)
+        return np.dot(X, w.T) + b
     
     def sigmoid(self, wx_b):
         # Probability F(X) for where Y = 1
@@ -19,7 +21,7 @@ class LogisticalRegression():
 
     def weights(self, X, Y, Y_h):
         m = X.shape[0]
-        return (1/m) * (X.T @ (sigmoid(X @ Y_) - Y))
+        return (1/m) * (X.T @ (Y_h - Y))
 
     def compute_weights_bias(self, w, b, tX, tY, vX, vY):
         P = self.sigmoid(self.linear_mod(w, tX, b))
@@ -34,47 +36,26 @@ class LogisticalRegression():
         p3 = 1 - P
         p4 = np.log(p3, where=((p3)>0))
         cost = -(p1+np.multiply(p2, p4))
-        return cost
+        return np.squeeze(cost)
     
-    def prediction(self, w, b, X):
+    def prediction(self, w, b, X, thresh=0.5):
         m = X.size[0]
         P = self.sigmoid(self.linear_mod(w, tX, b))
         Y_preds = np.zeros(1, m)
         for i in range(m):
-            if P[0, i] > 0.5:
-                Y_preds[0, i] = 1
+            if P[i, 0] > thresh:
+                Y_preds[i, 0] = 1
             else:
-                Y_preds[0, i] = 0
+                Y_preds[i, 0] = 0
         return Y_preds
 
     def calculate(self, w, b, tX, tY, vX, vY):
-        t_costs = []
-        v_costs = []
-        w = w
+        t_costs = list()
+        v_costs = list()
+
+        w = w.reshape((1,tX.shape[1]))
         b = b
-        for i in range(self.ITERATIONS):            
-#             Calculate gradient descent for training
-#             - this is linear model
-#             Calculate gradient descent for validation
-#             - this is linear model
-
-#             Calculate sigmold for training
-#             - pass above into sigmoid
-#             Calculate sigmold for validation
-#             - pass above info sigmoid 
-
-#             Calculate parameters (deriv weights/bias) with training data
-#             - pass in x train, y train, training sigmoid output
-
-#             Update weights and bias
-#             - update the weights and bias
-
-#             Compute mean log loss for training
-#             - compute with y train y training sigmoid 
-#             Compute mean log loss for validation
-#             - compute with y valid y valid sigmoid 
-
-#             Return losses and weights and bias 
+        for i in range(self.ITERATIONS):
             v_wxb = self.linear_mod(w, vX, b)
             t_wxb = self.linear_mod(w, tX, b)
             weight, beta = self.compute_weights_bias(w, b, tX, tY, vX, vY)
